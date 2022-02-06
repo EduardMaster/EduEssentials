b0cf1ad273027878b6b787f64b44d4cd4b4a9634
Eduard Dev <eduardkiller@hotmail.com>
2022-02-06 03:56:15 -0300

    EduEssentials RestartFake agora é Restart Real mais não efetua o Shutdown fica a critério da Host fazer isso

--- src/main/java/net/eduard/essentials/task/RestartTask.kt
+++ src/main/java/net/eduard/essentials/task/RestartTask.kt
@@ -3,77 +3,54 @@ package net.eduard.essentials.task
 import net.eduard.api.lib.manager.TimeManager
 import net.eduard.api.lib.modules.Extra
 import net.eduard.api.lib.modules.Mine
-import org.bukkit.Bukkit
 import java.util.*
 
 class RestartTask : TimeManager(60) {
 
-    enum class RestartFakeStage {
-        WAITING, PRE_RESTART, AFTER_RESTART, COMPLETE_RESTART
+    enum class RestartStage {
+        WAITING, RESTARTING, RESTARTED
     }
 
     @Transient
-    var stage = RestartFakeStage.WAITING
+    var stage = RestartStage.WAITING
     var selectedHour = 18
     var selectedMinute = 0
     var secondsIntoRestart = 30 * 60
-    var secondsIntoFinishRestart = 60 * 2
-    var messageWhenRestart = "§aO Servidor vai reiniciar aguarde alguns minutos ele irá reabrir."
     var messagePreRestart = "§cO Servidor irá reiniciar em %time , fique ligado para relogar no servidor em seguida."
+    var messageRestart = "§cO Servidor foi reiniciado."
 
     @Transient
     var usedTime = 60 * 30
 
     fun restart() {
+        stage = RestartStage.RESTARTED
+        Mine.console("§cO Servidor Reiniciou.")
         for (player in Mine.getPlayers()) {
-            player.kickPlayer(
-                messageWhenRestart.replace("/n", "\n")
-                    .replace("\\n", "\n")
-            )
+            player.sendMessage(messageRestart)
         }
-        Bukkit.setWhitelist(true)
-        stage = RestartFakeStage.AFTER_RESTART
-        usedTime = secondsIntoFinishRestart
-        Mine.console("§cO Servidor aplicou o reiniciar FAKE.")
     }
 
-    fun restartComplete() {
-        stage = RestartFakeStage.COMPLETE_RESTART
-        Bukkit.setWhitelist(false)
-        Mine.console("§aO Servidor saiu do reiniciar FAKE.")
-
-    }
 
     override fun run() {
-       val day =  Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-        val minute =  Calendar.getInstance().get(Calendar.MINUTE)
-        //Mine.console("§cHour of day: $day")
-       // Mine.console("§cMinute of Day: $minute")
-        if (stage == RestartFakeStage.PRE_RESTART) {
+        if (stage == RestartStage.WAITING) {
+            val day = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val minute = Calendar.getInstance().get(Calendar.MINUTE)
+            if (day == selectedHour &&
+                minute == selectedMinute) {
+                stage = RestartStage.RESTARTING
+                usedTime = secondsIntoRestart
+                Mine.console("§cO Servidor foi colocado para Reiniciar.")
+            }
+        } else if (stage == RestartStage.RESTARTING) {
             usedTime -= 60
             if (usedTime <= 0) {
                 restart()
             } else {
                 for (player in Mine.getPlayers()) {
-                    player.sendMessage(
-                        messagePreRestart
-                            .replace("/n", "\n")
-                            .replace("\\n", "\n")
-                            .replace("%time", Extra.formatTime(usedTime * 1000L))
+                    player.sendMessage(messagePreRestart
+                        .replace("%time", Extra.formatTime(usedTime * 1000L))
                     )
                 }
-            }
-        } else if (stage == RestartFakeStage.WAITING) {
-            if (Calendar.getInstance().get(Calendar.HOUR_OF_DAY) == selectedHour &&
-                Calendar.getInstance().get(Calendar.MINUTE) == selectedMinute) {
-                stage = RestartFakeStage.PRE_RESTART
-                usedTime = secondsIntoRestart
-                Mine.console("§cO Servidor foi colocado para reiniciar FAKE.")
-            }
-        } else if (stage == RestartFakeStage.AFTER_RESTART) {
-            usedTime -= 60
-            if (usedTime <= 0) {
-                restartComplete()
             }
         }
     }
