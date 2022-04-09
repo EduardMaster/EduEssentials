6301313e5e641d2a5ca26d923d3bc11c3dfb9939
Eduard Dev <eduardkiller@hotmail.com>
2022-04-09 16:40:23 -0300

    EduEssentials Por alguma razão o evento PlayerSpawnLocationEvent não funciona no FirstJoin ai precisa forçar teleportes no evento PlayerJoinEvent

--- src/main/java/net/eduard/essentials/listener/SpawnListener.kt
+++ src/main/java/net/eduard/essentials/listener/SpawnListener.kt
@@ -36,23 +36,47 @@ class SpawnListener : EventsManager() {
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    fun onSpawnLocationChange(e: PlayerSpawnLocationEvent) {
+    fun onSpawnJoin(e: PlayerSpawnLocationEvent) {
         val player = e.player
         val firstTime = !player.hasPlayedBefore()
+
+        if (EduEssentials.getInstance().getBoolean("spawn.teleport-on-first-join-only")){
+            if (!firstTime){
+                EduEssentials.getInstance().log("Primeira Entrada: Bloqueando jogador ${player.name} de Entrar no Local certo do Spawn")
+                return
+            }
+
+        }
         if (!firstTime) {
-            if (!player.hasPermission(EduEssentials.getInstance().getString("spawn.join-permission"))) return
-        } else {
-            if (EduEssentials.getInstance().getBoolean("spawn.teleport-on-first-join-only")) return
+            if (!player.hasPermission(EduEssentials.getInstance().getString("spawn.join-permission"))){
+                EduEssentials.getInstance().log("Sem Permissao: Bloqueando jogador ${player.name} de Entrar no Local certo do Spawn")
+                return
+            }
+
         }
         if (!EduEssentials.getInstance().storage.contains("spawn")) return
         val local = EduEssentials.getInstance().storage["spawn", Location::class.java]
         e.spawnLocation = local
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    @EventHandler(priority = EventPriority.MONITOR)
     fun onJoin(e: PlayerJoinEvent) {
         val player = e.player
-
+        val firstTime = !player.hasPlayedBefore()
+        if (!firstTime)return
+        if (!EduEssentials.getInstance().storage.contains("spawn")) return
+        val local = EduEssentials.getInstance().storage["spawn", Location::class.java]
+        player.teleport(local)
+        EduEssentials.getInstance().log("Primeira Entrada do ${player.name} Forcando movimento ate o Spawn")
+        EduEssentials.getInstance().syncDelay(5) {
+            player.teleport(local)
+        }
+        EduEssentials.getInstance().syncDelay(10) {
+            player.teleport(local)
+        }
+        EduEssentials.getInstance().syncDelay(15) {
+            player.teleport(local)
+        }
     }
 
 
