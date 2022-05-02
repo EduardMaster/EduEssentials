d3cb85690d660bc48e40ac4ea681926fa29f596f
Eduard Dev <eduardkiller@hotmail.com>
2022-05-01 23:15:41 -0300

    EduEssentials Usando Mutex na hora de aumentar o Logs do arquivo de logs do jogador

--- src/main/java/net/eduard/essentials/listener/PlayerActionsListener.kt
+++ src/main/java/net/eduard/essentials/listener/PlayerActionsListener.kt
@@ -1,35 +1,36 @@
 package net.eduard.essentials.listener
 
-import net.eduard.api.lib.kotlin.formatTime
+import com.google.common.util.concurrent.Monitor
 import net.eduard.api.lib.manager.EventsManager
-import net.eduard.api.lib.modules.Extra
 import net.eduard.essentials.EduEssentials
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
-import org.bukkit.event.player.PlayerQuitEvent
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.concurrent.CompletableFuture
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ThreadFactory
 
 class PlayerActionsListener : EventsManager() {
 
     val folder = File(EduEssentials.getInstance().dataFolder , "players/")
     val FORMAT_DATETIME = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+    private val mutex: Monitor = Monitor()
     init{
         folder.mkdirs()
     }
     fun newAction(player : Player , cancelled : Boolean , action : String){
         CompletableFuture.runAsync {
             try {
+                mutex.enter()
                 val file = File(folder, "${player.name.toLowerCase()}.txt")
                 file.appendText("\n${FORMAT_DATETIME.format(System.currentTimeMillis())} Cancelled:$cancelled ->$action")
             }catch (ex : Exception){
                 ex.printStackTrace()
+            }
+            finally {
+                mutex.leave()
             }
         }
     }
--- src/main/java/net/eduard/essentials/task/AutoMessagerTask.kt
+++ src/main/java/net/eduard/essentials/task/AutoMessagerTask.kt
@@ -3,11 +3,10 @@ package net.eduard.essentials.task
 import net.eduard.api.lib.manager.TimeManager
 import net.eduard.essentials.EduEssentials
 
-class AutoMessagerTask : TimeManager(
-    20L * EduEssentials.getInstance()
-    .configs.getLong("auto-message-per-seconds")) {
+class AutoMessagerTask : TimeManager(20L * EduEssentials.getInstance().configs.getLong("auto-message-per-seconds")) {
 
     var currentMessage = 0
+
     override fun run() {
         val messages = EduEssentials.getInstance().manager.autoMessages
         val msg = messages[currentMessage]
